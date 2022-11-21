8e6617678d75a79ea43a0aa217cab3bd818da211
Brenda Pereira <brendacamara@outlook.com>
2022-11-21 12:40:05 -0300

    add first screen

---
+++ src/scenes/App.java
@@ -0,0 +1,23 @@
+package scenes;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("initialScreen.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+        tela.getStylesheets().add(getClass().getResource("homeStyles.css").toExternalForm());
+        primaryStage.setTitle("tela login");
+        primaryStage.setScene(tela);
+        primaryStage.show();
+    }
+}
