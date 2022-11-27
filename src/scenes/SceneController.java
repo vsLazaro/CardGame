8baaa673053b9fc0f599768927e0972690af942f
Brenda Pereira <brendacamara@outlook.com>
2022-11-27 09:06:43 -0300

    add screen to enter player names

--- src/scenes/SceneController.java
+++ src/scenes/SceneController.java
@@ -1,11 +1,15 @@
 package scenes;
+
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -13,6 +17,12 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private Button enterButton;
+    @FXML
+    private TextField namePlayer2;
+
+    @FXML
+    private TextField namePlayer1;
 
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("initialScreen.fxml"));
@@ -23,10 +33,19 @@ public class SceneController {
     }
 
     public void switchToScene2(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("telaDois.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("nomeJogadores.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void enterGame(ActionEvent event) {
+        String namePlayer2Value= namePlayer2.getText();
+        String namePlayer1Value = namePlayer1.getText();
+        
+        System.out.println(namePlayer2Value);
+        System.out.println(namePlayer1Value);
+        
     }
 }
